9e65b7278378e991396fe9b3d6548e71c82cebac
joaobandarra <joaogsbandarra@gmail.com>
2017-06-30 15:46:25 +0100

    Add project files.

---
+++ SuperGes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperGes.Startup))]
+namespace SuperGes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
